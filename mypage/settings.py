77300562c00b8416eba2cef1cac845534d87cba4
Carnash.oz <carnash.oz@gmail.com>
2015-06-30 15:13:38 -0600

    Fix Setting.py

--- mypage/settings.py
+++ mypage/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.security.SecurityMiddleware',
 )
 
-ROOT_URLCONF = 'gettingstarted.urls'
+ROOT_URLCONF = 'mypage.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gettingstarted.wsgi.application'
+WSGI_APPLICATION = 'mypage.wsgi.application'
 
 
 # Database
