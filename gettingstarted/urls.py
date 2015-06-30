e1177c9fced05c155039dfc6b46359f33c8b61c1
Carnash.oz <carnash.oz@gmail.com>
2015-06-30 15:42:06 -0600

    Delete DB def page.

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -6,12 +6,7 @@ admin.autodiscover()
 import main.views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'gettingstarted.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^$', main.views.index, name='index'),
-    url(r'^db', main.views.db, name='db'),
     url(r'^admin/', include(admin.site.urls)),
 
 )
